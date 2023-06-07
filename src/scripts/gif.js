97b84b6e832fbc86246a49e52a3819d418154558
KriSob <kristina.soboleva@softwire.com>
2023-06-07 12:22:12 +0100

    Semantic - sections

--- src/scripts/gif.js
+++ src/scripts/gif.js
@@ -1,7 +1,7 @@
 fetch('https://api.giphy.com/v1/gifs/trending?api_key=T6LoHEjeXh8kuRbbLTs8FGmJoll68mZd&limit=10&rating=g')
 .then((response) => response.json())
 .then((data) => {
-  var mainBody = document.getElementsByClassName("content")[0];
+  var mainBody = document.getElementsByTagName("section")[0];
 
   data.data.forEach(gif => {
     var image = document.createElement("img");
